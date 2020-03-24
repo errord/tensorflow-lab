3d5c4a4ae8c6f149fdaf804307ac0279ccb4dacc
error.d <error.d@gmail.com>
2020-03-24 22:43:34 +0800

    up build

--- build_labX.sh
+++ build_labX.sh
@@ -2,3 +2,4 @@ bazel build -c dbg //tensorflow/cc:labX_test
 # ./bazel-bin/tensorflow/cc/labX_test
 # export TF_CPP_MIN_VLOG_LEVEL=5; ./bazel-bin/tensorflow/cc/labX_test --num_concurrent_sessions=1 --num_concurrent_steps=1 --num_iterations=1
 
+
